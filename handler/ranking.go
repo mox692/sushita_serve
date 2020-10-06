70dd6496e403eb49970b8180c865973ae5918430
mox692 <55653825+mox692@users.noreply.github.com>
2020-10-06 18:53:40 +0900

    gaeのtoken処理

--- handler/ranking.go
+++ handler/ranking.go
@@ -1,82 +1,21 @@
 package handler
 
 import (
-	"bytes"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 
 	"sushita_serve/db"
 
 	"golang.org/x/xerrors"
 )
 
-type LineOfLog struct {
-	RemoteAddr  string
-	ContentType string
-	Path        string
-	Query       string
-	Method      string
-	Body        string
-}
 type UserRanking struct {
 	ID       int
 	UserID   string
 	UserName string
 	Score    int
-}
-
-var TemplateOfLog = `
-Remote address:   {{.RemoteAddr}}
-Content-Type:     {{.ContentType}}
-HTTP method:      {{.Method}}
-
-path:
-{{.Path}}
-
-query string:
-{{.Query}}
-
-body:             
-{{.Body}}
-`
-
-// rankingエンドポイントを司るHandler
-// rにリクエスト情報を詰めて、処理の中で生じたerr等はwに書き込む。
-func RankingGet(w http.ResponseWriter, r *http.Request) {
-
-	// wの動作確認。
-	fmt.Fprintf(w, "Hello, World\n")
-	fmt.Fprintf(w, "Request: %v\n", r)
-
-	// リクエストbodyの内容を整形してwに書き込み
-	bufbody := new(bytes.Buffer)
-	bufbody.ReadFrom(r.Body)
-	log.Println(bufbody, "\n=====================")
-	body := bufbody.String()
-	log.Println(body, "\n=====================")
-
-	line := LineOfLog{
-		r.RemoteAddr,
-		r.Header.Get("Content-Type"),
-		r.URL.Path,
-		r.URL.RawQuery,
-		r.Method, body,
-	}
-	tmpl, err := template.New("line").Parse(TemplateOfLog)
-	if err != nil {
-		panic(err)
-	}
-
-	bufline := new(bytes.Buffer)
-	err = tmpl.Execute(bufline, line)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Printf(bufline.String())
 }
 
 func GetRanking(w http.ResponseWriter, r *http.Request) {
@@ -88,18 +27,19 @@ func GetRanking(w http.ResponseWriter, r *http.Request) {
 	_, err := selectUserByToken(token)
 	if err != nil {
 		log.Fatal("%s", err)
-		return
 	}
 	fmt.Printf("token: %s\n", token)
 
 	userRankings, err := selectAllRankingData()
 	if err != nil {
 		log.Fatal("%s", err)
-		return
 	}
 
-	fmt.Fprintf(w, "1位の名前: %s\n", userRankings[0].UserName)
-	fmt.Fprintf(w, "2位の名前: %s\n", userRankings[1].UserName)
+	for i := 0; i < len(userRankings); i++ {
+		fmt.Fprintf(w, "%d: %s\n", i+1, userRankings[0].UserName)
+	}
+	// fmt.Fprintf(w, "1位の名前: %s\n", userRankings[0].UserName)
+	// fmt.Fprintf(w, "2位の名前: %s\n", userRankings[1].UserName)
 }
 
 func selectAllRankingData() ([]*UserRanking, error) {
@@ -124,7 +64,7 @@ func convertToRanking(rows *sql.Rows) ([]*UserRanking, error) {
 }
 
 func selectUserByToken(token string) (*UserRanking, error) {
-	row := db.Conn.QueryRow("select * from user_ranking where uesr_id = ?", token)
+	row := db.Conn.QueryRow("select * from user_ranking where user_id = ?", token)
 	return convertToUserRanking(row)
 }
 
